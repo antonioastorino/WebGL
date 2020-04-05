bc257702cc53aa9a41d1d5f2025b3778a940c36e
Antonio <antonio_jazz@yahoo.it>
2020-04-05 16:49:54 +0200

    Time class returns time intervals in seconds

--- template/src/base/XYZTime.ts
+++ template/src/base/XYZTime.ts
@@ -1,10 +1,11 @@
 export class XYZTime {
 	private static _currTime: number = performance.now();
 
+	// returns time intervals in seconds
 	public static get deltaTime(): number {
 		let newTime = performance.now()
 		let delta = newTime - this._currTime;
 		this._currTime = newTime;
-		return delta;
+		return delta / 1000;
 	}
 }
